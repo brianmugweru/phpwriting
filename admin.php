fd53784e8c329dd428e7a1ef4650a658bf4bdee9
Brian Muugweru <brianmugweru@gmail.com>
2016-11-02 11:23:40 +0300

    add query links to page to direct to work specifics

--- admin.php
+++ admin.php
@@ -4,11 +4,6 @@
   if(!$sql){
     echo "could not pick up any data from the database";
   }
-  $sql2 = mysqli_query($db, "SELECT username FROM Users WHERE role = 'normal'");
-  if(!$sql2){
-    echo "could not retrive select data for users".mysqli_error($db);
-  }
-
 ?>
 <html>
 <head>
@@ -44,7 +39,7 @@
       <td><?php echo $row["language"];?></td>
       <td><?php echo $row["upload_date"];?></td>
       <td><?php echo $row["file_uploads"];?></td>
-      <td><button class="button" value="<?php echo $row['ownership']." ".$row['id'] ?>" data-reveal-id="assign">Assign job</button></td>
+      <td><a href="viewspecs.php?job_id=<?php echo $row["id"] ?>" class="button">Assign job</a></td>
 <tr>
 <?php
     }
@@ -52,36 +47,6 @@
 ?>
 </tr>
 </table>
-<div id="assign" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
-<h1><center>Assign users</center></h1>
-  <form action="assign.php" method="post">
-<?php
-  if(mysqli_num_rows($sql2) > 0){
-    while($row = mysqli_fetch_assoc($sql2)){
-?>
-  <input type="radio" name="username" value="<?php echo $row["username"] ?>" id="<?php echo $row["username"] ?>" required><label for="<?php echo $row["username"] ?>"><?php echo $row["username"] ?></label><br>
-<?php
-    }
-  }
-?>
-    <input type="hidden" name="job_id" class="jobid">
-    <input type="submit" name="submit" value="assign selected user">
-  </form>
-  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
-</div>
-<script type="text/javascript">
-  $(document).ready(function() {
-    $('button').click(function() {
-      var id = $(this).val(); 
-      var split = id.split(" ", 2);
-      console.log("button clicked:"+split[1]);
-      var owner = split[0];
-      $(".jobid").val(split[1]);
-      $("#owner").html(split[0]);
-    });
-  });
-</script>
-
 <script src = "assets/bower_components/foundation/js/foundation.js"></script>
 <script src = "assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
 <script>
