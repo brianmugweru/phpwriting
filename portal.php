42eb56919245022d5c41265be7723cd0abf9f63d
Brian Muugweru <brianmugweru@gmail.com>
2017-01-05 00:50:21 +0300

    add header and top-bar for user portal

--- portal.php
+++ portal.php
@@ -1,2 +1,47 @@
-
 <?php include("resources/templates/dashboard/masthead.html"); ?>
+
+<div class="row column header">
+    <h4>Academia Zone</h4>
+    <nav role="navigation" class="top-bar" data-topbar>
+      <ul class="title-area">
+        <li class="name"></li>
+        <li class="toggle-topbar menu-icon">
+          <a href="#">
+            <span></span>
+          </a>
+        </li>
+
+      </ul>
+      <section class="top-bar-section">
+        <ul class="left">
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="active">
+            <a href="#">jobs</a>
+          </li>
+          <li>
+            <a href="#">writers</a>
+          </li>
+          <li>
+            <a href="#">Order Form</a>
+          </li>
+          <li>
+            <a href="#">home</a>
+          </li>
+        </ul>
+    
+        <ul class="right">
+          <li>
+            <a href="#">My users</a>
+          </li>
+          <li>
+            <a href="#">My Profile</a>
+          </li>
+          <li>
+            <a href="#">My Account</a>
+          </li>
+        </ul>
+      </section>
+    </nav>
+</div>
