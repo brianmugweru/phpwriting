b3e953f1e73c8b50958a7721d227a59cf74c0aca
Brian Muugweru <brianmugweru@gmail.com>
2017-01-06 08:26:08 +0300

    break portal.php into several templates for easy management

--- portal.php
+++ portal.php
@@ -1,63 +1,16 @@
+<!-- Master head for dashboard -->
 <?php include("resources/templates/dashboard/masthead.html"); ?>
+<!-- Master head for dashboard ends here -->
 
-<div class="row header">
-    <h4>Academia Zone</h4>
-    <nav role="navigation" class="top-bar" data-topbar>
-      <ul class="title-area">
-        <li class="name"></li>
-        <li class="toggle-topbar menu-icon">
-          <a href="#">
-            <span></span>
-          </a>
-        </li>
-
-      </ul>
-      <section class="top-bar-section">
-        <ul class="left">
-          <li>
-            <a href="#">Dashboard</a>
-          </li>
-          <li class="active">
-            <a href="#">Manage Orders</a>
-          </li>
-          <li>
-            <a href="#">users</a>
-          </li>
-          <li>
-            <a href="#">Writers</a>
-          </li>
-          <li>
-            <a href="#">Clients</a>
-          </li>
-        </ul>
-    
-        <ul class="right">
-          <li>
-            <a href="#">My users</a>
-          </li>
-          <li>
-            <a href="#">My Profile</a>
-          </li>
-          <li>
-            <a href="#">My Account</a>
-          </li>
-        </ul>
-      </section>
-    </nav>
-</div>
-<div class="row head">
-  <div><h3>Dashboard</h3></div>
-<div class="medium-2 columns sidebar" data-equalizer-watch>
-    <img src="" alt="" />
-    <p>admin</p>
-  </div>
-  <div class="medium-10 columns main">
-    <div class="start">dashboard</div>
-    <div class="end">what
-    </div>
-  </div>
-
-</div>
-
+  <!-- Header for dashboard with topbar -->
+<?php require("resources/templates/dashboard/navbar.html"); ?>
+  <!-- Header for dashboard ends here -->
+<!-- Dashboard for layout -->
+<?php require("resources/templates/dashboard/dash.html"); ?>
+<!-- Dashboard ends for layout -->
 </body>
 </html>
+<?php function dashboard(){
+  echo "welcome";
+}
+?>
