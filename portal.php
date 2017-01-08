8e2380424d4c4939b298a401730966bf36ff4c7d
Brian Muugweru <brianmugweru@gmail.com>
2017-01-08 10:19:52 +0300

    add dashboard function to layout for easy templating of php

--- portal.php
+++ portal.php
@@ -1,3 +1,10 @@
+<!-- require session page to ensure page works -->
+<?php require("resources/library/session.php"); ?>
+<?php
+  $pagename="portal";
+  $pagetab = "main";
+?>
+<!-- Session required-->
 <!-- Master head for dashboard -->
 <?php include("resources/templates/dashboard/masthead.html"); ?>
 <!-- Master head for dashboard ends here -->
@@ -11,7 +18,4 @@
 <!-- Dashboard footer begins -->
 <?php require("resources/templates/dashboard/footer_scripts.html"); ?>
 <!-- Dashboard footer ends here -->
-<?php function dashboard(){
-  echo "welcome";
-}
-?>
+
