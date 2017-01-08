9f680b4c5eee3cfe3765f9e9a3b2f4aab01835ec
Brian Muugweru <brianmugweru@gmail.com>
2017-01-08 10:13:38 +0300

    change scripts to reference templates and function for viewing jobs from the database according to their status

--- admin.php
+++ admin.php
@@ -1,51 +1,9 @@
 <?php
-  include("resources/library/session.php");
-  $sql = mysqli_query($db, "SELECT * FROM jobs WHERE status = 'uploaded'");
-  if(!$sql){
-    echo "could not pick up any data from the database";
-  }
+$definestatus = 'uploaded';
+$pagetitle = 'Currently uploaded jobs';
+$page = 'manage';
+$pagetab = 'jobs';
+  include("resources/templates/dashboard/status.php");
+  include("portal.php");
 ?>
-</head>
-<body>
-<h1>jobs in the database</h1>
-<a href="logout.php">logout</a>
-<a href="completed.php">Finished</a>
-<a href="repeat.php">adminrejected</a>
-<table>
-<tr>
-<td>topic</td>
-<td>type</td>
-<td>subject</td>
-<td>pages</td>
-<td>deadline</td>
-<td>language</td>
-<td>upload date</td>
-<td>file upload</td>
-<td>Assign job</td>
-</tr>
-<?php
-  if(mysqli_num_rows($sql) > 0){
-    while($row = mysqli_fetch_assoc($sql)){
-?>
-<tr>
-      <td><?php echo $row["topic"];?></td>
-      <td><?php echo $row["type"];?></td>
-      <td><?php echo $row["subject"];?></td>
-      <td><?php echo $row["pages"];?></td>
-      <td><?php echo $row["ownership"];?></td>
-      <td><?php echo $row["language"];?></td>
-      <td><?php echo $row["upload_date"];?></td>
-      <td><?php echo $row["file_uploads"];?></td>
-      <td><a href="viewspecs.php?job_id=<?php echo $row["id"] ?>" class="button">Assign job</a></td>
-</tr>
-<?php
-    }
-  }
-?>
-</table>
-<script src = "assets/bower_components/foundation/js/foundation.js"></script>
-<script src = "assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script> <script> Foundation.global.namespace = '';
-  $(document).foundation();
-</script>
-</body>
-</html>
+ 
--- repeat.php
+++ repeat.php
@@ -1,55 +1,10 @@
 <?php
-  require("session.php");
-  $sql = mysqli_query($db, "SELECT * FROM jobs WHERE status = 'adminpoor'");
-  if(!$sql){
-    echo "could not pick up any data from the database";
-  }
+  $definestatus = 'adminpoor';
+  include("resources/templates/dashboard/status.php");
+  include("portal.php");
 ?>
-<html>
-<head>
-<title>view jobs</title>
-<link type="text/css" href="assets/bower_components/foundation/css/foundation.min.css" rel="stylesheet">
-<script src = "assets/bower_components/foundation/js/vendor/jquery.js"></script>
-</head>
-<body>
-<h1>jobs in the database</h1>
-<a href="logout.php">logout</a>
-<a href="completed.php">Finished.php</a>
-<table>
-<tr>
-<td>topic</td>
-<td>type</td>
-<td>subject</td>
-<td>pages</td>
-<td>deadline</td>
-<td>language</td>
-<td>upload date</td>
-<td>file upload</td>
-<td>Assign job</td>
-</tr>
-<?php
-  if(mysqli_num_rows($sql) > 0){
-    while($row = mysqli_fetch_assoc($sql)){
-?>
-<tr>
-      <td><?php echo $row["topic"];?></td>
-      <td><?php echo $row["type"];?></td>
-      <td><?php echo $row["subject"];?></td>
-      <td><?php echo $row["pages"];?></td>
-      <td><?php echo $row["ownership"];?></td>
-      <td><?php echo $row["language"];?></td>
-      <td><?php echo $row["upload_date"];?></td>
-      <td><?php echo $row["file_uploads"];?></td>
-      <td>Repeat job</td>
-</tr>
-<?php
-    }
-  }
-?>
-</table>
-<script src = "assets/bower_components/foundation/js/foundation.js"></script>
-<script src = "assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script> <script> Foundation.global.namespace = '';
-  $(document).foundation();
-</script>
-</body>
-</html> 
+<!--//
+
+Need to clear the assign job part according to the status on the page for better templating of the application
+     <td>Repeat job</td> -->
+
