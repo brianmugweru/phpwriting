c72466b43ada29f0b039ac088f60f35fe6f06d65
Brian Muugweru <brianmugweru@gmail.com>
2017-01-10 21:18:23 +0300

    change from jobs to orders of page title

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 $definestatus = 'uploaded';
-$pagetitle = 'Currently uploaded jobs';
+$pagetitle = 'Currently Uploaded Orders';
 $page = 'manage';
 $pagetab = 'jobs';
   include("resources/templates/dashboard/status.php");
