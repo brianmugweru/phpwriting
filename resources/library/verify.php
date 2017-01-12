2b9135d8aceee47955e04a38b41ad1215c2efe8c
Brian Muugweru <brianmugweru@gmail.com>
2017-01-12 10:27:13 +0300

    change location directory to ../../admin page

--- resources/library/verify.php
+++ resources/library/verify.php
@@ -3,5 +3,5 @@
   $job_id = $_POST['job_id'];
   $adminsay = $_POST['verify'];
   $sql = mysqli_query($db, "UPDATE jobs SET status = '$adminsay' WHERE id = '$job_id'");
-  header("location:admin.php");
+  header("location:../../admin.php");
 ?>
