65ed71920f557699a1415dee29ba3a26fcad5385
Brian Muugweru <brianmugweru@gmail.com>
2017-01-21 19:06:50 +0300

    whatever

---
+++ checkupload.php
@@ -0,0 +1,11 @@
+<?php
+  $page = "dashboard";
+  $pagetitle = "Probably pay for upload";
+  $userstatus = "current";
+  $orderstatus = "uploaded";
+  $stgood = "";
+  $stbad = "";
+  include("resources/templates/dashboard/check.php");
+  include("portal.php");
+?>
+
